98d0c127b34b69c010159df1888beca1f5d6230f
Atabek <atabekmur@gmail.com>
2017-10-09 23:36:40 +1100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+	$(document).ready(function(){
+		$(".rotate").textrotator({
+			  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+			  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+			  speed: 4000 // How many milliseconds until the next word show.
+			});
+	});
