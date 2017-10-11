f05b63376d5df732999929fdc10f56170214da82
Atabek <atabekmur@gmail.com>
2017-10-11 22:45:17 +1100

    Implement first version of APIs

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import os
 import sqlite3
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, jsonify
 
 app = Flask(__name__)
 app.debug = True
 
-DATABASE = os.getcwd() + '/sozlik.db'
+DATABASE = '/Users/atabek/Projects/Flask/sozlik.com/sozlik/sozlik.db'
 
 
 def get_db():
@@ -28,14 +27,37 @@ def index():
     return render_template('index.html')
 
 
+# /api/translation?word=adam
 @app.route("/api/translation")
 def api_get_translation():
-    cur = get_db().cursor()
-    cur.execute("select * from qqen")
-    result = cur.fetchone()
-    return render_template("word.html", result=result)
+    if request.method == 'GET':
+        search_word = request.args.get('word')
+        cur = get_db().cursor()
+        cur.execute("select * from qqen where word = ?", (search_word,))
+        result = cur.fetchone()
+        return jsonify(id=result["id"], word=result["word"], translation=result["translation"])
 
 
-# /api/translation?word=adam
 # /api/suggestion?begins=a
+@app.route("/api/suggestion")
+def api_get_suggestion():
+    if request.method == 'GET':
+        search_word = request.args.get('begins')
+        cur = get_db().cursor()
+        cur.execute("select word from qqen where word like ? limit 10", (search_word + '%',))
+        result = cur.fetchall()
+        data = []
+        for r in result:
+            data.append(r["word"])
+        return jsonify(suggestions=data)
+
+
 # /translate?word=adam
+@app.route("/translate")
+def get_translate():
+    if request.method == 'GET':
+        search_word = request.args.get('word')
+        cur = get_db().cursor()
+        cur.execute("select * from qqen where word = ?", (search_word,))
+        result = cur.fetchone()
+        return render_template("translate.html", result=result)
