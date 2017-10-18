4b08f5aadb33cb857c7417cc0066551436af2054
Atabek <atabekmur@gmail.com>
2017-10-18 18:30:26 +1100

    Add did_you_mean option in details page

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sqlite3
 
 from flask import Flask, render_template, g, jsonify
 
+from spell import candidates
+
 app = Flask(__name__)
 
 DATABASE = '/Users/atabek/Projects/Flask/sozlik.com/sozlik/sozlik.db'
@@ -46,15 +48,26 @@ def api_get_suggestion(beginswith):
 @app.route("/translate/<search_word>")
 def get_translate(search_word):
     search_word = normalize_query(search_word)
-    print(search_word)
     cur = get_db().cursor()
     cur.execute("select * from qqen where word = ?", (search_word,))
     result = cur.fetchone()
     if result:
         return render_template("translate.html", word=result["word"], translation=result["translation"])
     else:
-        return render_template("notfound.html", word=search_word)
+        did_you_mean = candidates(search_word, get_all_words())
+        return render_template("notfound.html", word=search_word, did_you_mean=did_you_mean)
 
 
 def normalize_query(search_word):
     return re.sub('[^a-z\-]', '', search_word.lower())
+
+
+def get_all_words():
+    data = []
+    cur = get_db().cursor()
+    cur.execute("select word from qqen")
+    result = cur.fetchall()
+    if result:
+        for r in result:
+            data.append(r["word"])
+    return data
---
+++ spell.py
@@ -0,0 +1,34 @@
+# Spellchecker is based on implementation by
+# Peter Norvig at http://norvig.com/spell-correct.html
+from collections import Counter
+
+
+WORDS = None
+
+
+def candidates(word, all_words):
+    """Generate possible spelling corrections for word."""
+    global WORDS
+    WORDS = Counter(all_words)
+    return known([word]) or known(edits1(word)) or known(edits2(word))
+
+
+def known(words):
+    """The subset of `words` that appear in the dictionary of WORDS."""
+    return set(w for w in words if w in WORDS)
+
+
+def edits1(word):
+    """All edits that are one edit away from `word`."""
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [L + R[1:] for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+    inserts = [L + c + R for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+
+def edits2(word):
+    """All edits that are two edits away from `word`."""
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
