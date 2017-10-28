98989234bde1c712a322e6ba236ac146ab8c9b62
Atabek <atabekmur@gmail.com>
2017-10-29 08:33:31 +1100

    Add karakalpak letters to be used in 'did you mean' feature

--- util/spell.py
+++ util/spell.py
@@ -20,7 +20,7 @@ def known(words):
 
 def edits1(word):
     """All edits that are one edit away from `word`."""
-    letters = 'abcdefghijklmnopqrstuvwxyz'
+    letters = 'aábcdefgǵhiıjklmnńoópqrstuúvwxyz'
     splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
     deletes = [L + R[1:] for L, R in splits if R]
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
